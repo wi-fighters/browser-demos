42a4283209d639af0739e084965a4d5664c3b05f
Eva Larumbe <evalarumbe@gmail.com>
2020-01-22 14:43:29 +0100

    Apply CSS classes to groups of styles

--- script.js
+++ script.js
@@ -2,5 +2,19 @@
     // target the ul
     const mainMenu = document.querySelector('ul');
     // give it a blue class
-    mainMenu.className = 'blue';
+    // mainMenu.className = 'blue';
+    const makeBluePink = () => {
+        mainMenu.classList.remove('purple');
+        mainMenu.classList.add('blue', 'pink-background');
+        // mainMenu.classList.add('');
+    };
+
+    const makePurple = () => {
+        mainMenu.classList.add('purple');
+        mainMenu.classList.remove('blue');
+        mainMenu.classList.remove('pink-background');
+    };
+
+    makeBluePink();
+    // makePurple();
 })();
