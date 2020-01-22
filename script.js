2327d8f9f709fe3e71dbc51e977b3218dcf37da7
Eva Larumbe <evalarumbe@gmail.com>
2020-01-22 15:04:17 +0100

    Mix and match menus and styles

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 (() => {
     // target the ul
-    const mainMenu = document.querySelector('ul');
+    const mainMenu = document.querySelector('#main');
+    const secondaryMenu = document.querySelector('#secondary');
+
     // give it a blue class
     // mainMenu.className = 'blue';
-    const makeBluePink = () => {
-        mainMenu.classList.remove('purple');
-        mainMenu.classList.add('blue', 'pink-background');
-        // mainMenu.classList.add('');
+    const styleHeaderMenu = menu => {
+        menu.classList.remove('purple');
+        menu.classList.add('blue', 'pink-background');
     };
 
-    const makePurple = () => {
-        mainMenu.classList.add('purple');
-        mainMenu.classList.remove('blue');
-        mainMenu.classList.remove('pink-background');
+    const styleDrawerMenu = menu => {
+        menu.classList.add('purple');
+        menu.classList.remove('blue');
+        menu.classList.remove('pink-background');
     };
 
-    makeBluePink();
-    // makePurple();
+    styleHeaderMenu(secondaryMenu);
+    styleDrawerMenu(mainMenu);
 })();
