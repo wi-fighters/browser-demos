8858d22d6aa75582cea2d01f3141c29265c4c731
Eva Larumbe <evalarumbe@gmail.com>
2020-01-28 11:37:32 +0100

    Tidy code + comments

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 (() => {
-    // 1. identify which element will emit the event (what are we detecting?)
+    // 1. identify which elements will emit the event (what are we detecting?)
     const buttons = [...document.querySelectorAll('[role=button]')];
 
     // 2. define the handler function (what code should run when the event happens?)
+    // USE ES5 (old-school) FUNCTION EXPRESSION SYNTAX WITH EVENT HANDLERS
+    // This is so `this` refers to the element that emitted the event.
+    // (with arrow syntax, `this` ends up being the Window, which is not helpful)
     const setLoadState = function() {
 
         // target the thing we want to change (the same div that got clicked)
-        const square = this;
-        console.log('ES5 function expression');
-        console.log(this);
+        const elementThatGotClicked = this;
+        // If you're getting unexpected results, try logging `this`
+        // console.log(this);
 
-        const squareClasses = [...square.classList];
-        console.log('includes success?',  squareClasses.includes('success'));
+        const buttonClasses = [...elementThatGotClicked.classList];
+        console.log('includes success?',  buttonClasses.includes('success'));
 
-        // check which class it has and replace it with the other one
-        // (for this example, assume it just has one class)
-
-        // console.log('before:', square.className);
-
-        if (squareClasses.includes('success')) {
-            square.classList.replace('success', 'error');
+        if (buttonClasses.includes('success')) {
+            elementThatGotClicked.classList.replace('success', 'error');
         } else {
-            square.classList.replace('error', 'success');
+            elementThatGotClicked.classList.replace('error', 'success');
         }
-
-        // console.log('after:', square.className);
         
         // TODO: consider using the toggle method instead?
     };
